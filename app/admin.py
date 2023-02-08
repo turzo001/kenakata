38cb5cfe8077f9625579f798472f7d3084b98d44
turzo001 <iammonzurmorshed@gmail.com>
2023-02-08 15:56:55 +0600

    Second day

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import(
+    Customer,
+    Cart,
+    Product,
+    Cart,
+    Orderplaced
+)
 
-# Register your models here.
+#costomer register
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','locality','city','zipcode','state']
+
+# Product resister
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id','title','selling_price','discounted_price','description','brand',
+                  'catagory','product_image']
+
+#cart resister
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','product','quantity']
+
+#orderplace resister
+@admin.register(Orderplaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','quantity','ordered_date','status']
+
+ 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator,MinValueValidator
+STATE_CHOICES=(
+    ('Barisal Division','Barisal Division'),
+    ('Chittagong Division','Chittagong Division'),
+    ('Dhaka Division','Dhaka Division'),
+    ('Mymendingh Division','Mymendingh Division'),
+    ('Khulna Division','Khulna Division'),
+    ('Rajshahi Division','Rajshahi Division'),
+    ('Rangpur Division','Rangpur Division'),
+    ('Sylhet Division','Sylhet Division'),
+)
+class Customer(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=200)
+    locality= models.CharField(max_length=200)
+    city= models.CharField(max_length=50)
+    zipcode= models.IntegerField()
+    state= models.CharField(choices=STATE_CHOICES,max_length=50)
+    
+    def __str__(self):
+        return str(self.id)
+CATAGORY_CHOICES=(
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear'),
+)
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description=models.TextField()
+    brand=models.CharField(max_length=100)
+    catagory=models.CharField(choices=CATAGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='productimg')
+    
+    def __str__(self):
+        return str(self.id)
+    
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity= models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
+STATUS_CHOICES=(
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+)
 
-# Create your models here.
+class Orderplaced(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    ordered_date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='pending')

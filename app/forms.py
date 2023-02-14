d6ae8f7530caed0851e387aa5e98bfd73b214e2f
turzo001 <iammonzurmorshed@gmail.com>
2023-02-14 23:53:44 +0600

    14feb11:53

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,Userna
 from django.contrib.auth.models import User
 from django.utils.translation import gettext,gettext_lazy as _
 from django.contrib.auth import password_validation
+from .models import Customer
 class CustomerRegistrationForm(UserCreationForm):
     password1=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2=forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
@@ -34,4 +35,13 @@ class MySetPasswordForm(SetPasswordForm):
     strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),
     help_text=password_validation.password_validators_help_text_html())
     new_password2=forms.CharField(label=("Confirm New Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
-    
+
+class CustomerProfileForm(forms.ModelForm):
+    class Meta:
+        model=Customer
+        fields= ['name','locality','city','state','zipcode']
+        widgets={'name':forms.TextInput(attrs={'class':'form-control'}),
+                 'locality':forms.TextInput(attrs={'class':'form-control'}),
+                 'city':forms.TextInput(attrs={'class':'form-control'}),
+                 'state':forms.Select(attrs={'class':'form-control'}),
+                 'zipcode':forms.NumberInput(attrs={'class':'form-control'})}
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer, Product, Cart, Orderplaced
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 # def home(request):
 #  return render(request, 'app/home.html')
@@ -18,11 +18,6 @@ class ProductView(View):
         'bottomwears':bottomwears,
         'mobiles':mobiles})
     
-
-
-# def product_detail(request):
-#  return render(request, 'app/productdetail.html')
-
 class ProductDetailView(View):
     def get(self,request,pk):
         product=Product.objects.get(pk=pk)
@@ -34,9 +29,6 @@ def add_to_cart(request):
 
 def buy_now(request):
     return render(request, 'app/buynow.html')
-
-def profile(request):
-    return render(request, 'app/profile.html')
 
 def address(request):
     return render(request, 'app/address.html')
@@ -110,4 +102,22 @@ class CustomerRegistrationView(View):
                       {'form':form}) 
 def checkout(request):
     return render(request, 'app/checkout.html')
+
+class ProfileView(View):
+    def get(self,request):
+        form= CustomerProfileForm()
+        return render(request, 'app/profile.html',{'form':form,'active':'btn-primary'})
+    def post(self,request):
+        form= CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr=request.user
+            name=form.cleaned_data['name']
+            locality=form.cleaned_data['locality']
+            city=form.cleaned_data['city']
+            state=form.cleaned_data['state']
+            zipcode=form.cleaned_data['zipcode']
+            reg=Customer(user=usr,name=name,locality=locality,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'congratulations profile updated successfully!!')
+        return render(request,'app/profile.html',{'form':form,'active':'btn-primary'})
 

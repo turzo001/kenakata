cbf312bf32e67d12626bc1cdd53f6ae98bba04a8
turzo001 <iammonzurmorshed@gmail.com>
2023-02-15 15:29:58 +0600

    15feb3:29

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .models import Customer, Product, Cart, Orderplaced
 from .forms import CustomerRegistrationForm,CustomerProfileForm
@@ -25,13 +25,36 @@ class ProductDetailView(View):
         {'product':product})
 
 def add_to_cart(request):
-    return render(request, 'app/addtocart.html')
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product= Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        cart=Cart.objects.filter(user=user)
+        amount=0.0
+        shipping_amount=70.0
+        total_amount=0.0
+        cart_product=[p for p in Cart.objects.all() if p.user == user]
+        
+        if cart_product:
+            for p in cart_product:
+                tempamount=(p.quantity*p.product.discounted_price)
+                amount += tempamount
+                totalamount= amount + shipping_amount
+            return render(request,'app/addtocart.html',{'carts':cart,'totalamount':totalamount,'amount':amount})
+        else:
+            return render(request, 'app/emptycart.html')
 
 def buy_now(request):
     return render(request, 'app/buynow.html')
 
 def address(request):
-    return render(request, 'app/address.html')
+    add= Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html',{'add':add,'active':'btn-primary'})
 
 def orders(request):
     return render(request, 'app/orders.html')

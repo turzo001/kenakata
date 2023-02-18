62d44ada09af27d58a3c99099fc767d2a2a4df07
turzo001 <iammonzurmorshed@gmail.com>
2023-02-19 02:27:33 +0600

    19feb2:27

--- app/forms.py
+++ app/forms.py
@@ -22,7 +22,7 @@ class LoginForm(AuthenticationForm):
 
 class MyPasswordChangeForm(PasswordChangeForm):
     old_password=forms.CharField(label=_("old password"),strip=False,
-                                 widget=forms.PasswordInput(attrs={'autocomplete':'corrent-password','autofocus':True,'class':'form-control'}))
+                                 widget=forms.PasswordInput(attrs={'autocomplete':'current-password','autofocus':True,'class':'form-control'}))
     new_password1=forms.CharField(label=_("new password"),strip=False,
                                  widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
     new_password2=forms.CharField(label=_("new password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new password','class':'form-control'}))
